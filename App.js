8ef2ebc17a19054981fd7f2aef48ef0c596a8a89
Nicholas Veras <nicholasveras@MacBook-Air-de-Nicholas.local>
2023-12-19 12:58:32 -0300

    trainingSlider

--- App.js
+++ App.js
@@ -1,10 +1,26 @@
+import React, {useState} from 'react';
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import Slider from '@react-native-community/slider';
 
 export default function App() {
+  const [valor, setValor] = useState(50)
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      
+      <Slider
+      minimumValue={0}
+      maximumValue={100}
+      value={valor}
+      onValueChange={(valorSelecionado) => setValor(valorSelecionado)}
+      minimumTrackTintColor='#000FFF'
+      maximumTrackTintColor='#FF0000'
+      thumbTintColor='#121212'
+      />
+
+      <Text style={{textAlign: 'center', fontSize: 25}}>Valor: {valor.toFixed(0)}</Text>
+
       <StatusBar style="auto" />
     </View>
   );
